27dc82c3875aa6c91f4b0cc95b14796f12a0fe7b
Alex <muirkat138@gmail.com>
2021-11-08 21:36:38 -0700

    final

--- Program.cs
+++ Program.cs
@@ -6,20 +6,21 @@ namespace ArchitectArithmetic
     {
         static void Main(string[] args)
         {
-             double totalArea = Rect(2500, 1500) + Triangle(500, 750) + 0.5 * Circle(375);
-      Console.WriteLine(totalArea);
+            double totalArea = Rect(2500, 1500) + Triangle(500, 750) + 0.5 * Circle(375);
+            double totalCost = totalArea * 180;
+            Console.WriteLine($"This will all cost {Math.Round(totalCost, 2)} pesos");
         }
         static double Rect(double length, double width)
         {
-        return length * width;
+            return length * width;
         }
          static double Circle(double radius)
-    {
-      return Math.PI * Math.Pow(radius, 2);
-    }
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
 
-      static double Triangle(double bottom, double height){
-      return 0.5 * bottom * height;
-    }
+        static double Triangle(double bottom, double height){
+            return 0.5 * bottom * height;
+        }
     }
 }
